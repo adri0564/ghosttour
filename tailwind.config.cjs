b0bdb14d6269defe81fe976b7a1c0ef34b3f79a4
Adrian Soelberg <adri0564@stud.kea.dk>
2023-05-22 16:50:01 +0200

    highlight section opret

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        "dm-sans": ["DM Sans", "sans-serif"],
+        "special-elite": ["Special Elite", "cursive"],
+      },
+      fontWeight: {
+        regular: 400,
+        medium: 500,
+        bold: 700,
+      },
+      colors: {
+        "brand-color": "#c01111",
+        "primary-text": "#f8f6f2",
+      },
+    },
+  },
+  plugins: [],
+};
