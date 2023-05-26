7254b4884f95f9fad37e810d1ebec5ef7f93619c
Adrian Soelberg <adri0564@stud.kea.dk>
2023-05-26 18:53:02 +0200

    custom skriftstørrelser + farver + sizing start

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -12,9 +12,33 @@ module.exports = {
         medium: 500,
         bold: 700,
       },
+      fontSize: {
+        small: "15px",
+        base: "18px",
+        large: "23px",
+        xlarge: "28px",
+        xxlarge: "35px",
+        xxxlarge: "55px",
+        xxxxlarge: "85px",
+      },
+
       colors: {
         "brand-color": "#c01111",
         "primary-text": "#f8f6f2",
+        "primary-color": "#f8f6f2",
+        "secondary-color": "#0e0e10",
+        "tertiary-color": "#9da3ae",
+      },
+
+      spacing: {
+        xxsmall: "2px",
+        xsmall: "4px",
+        small: "8px",
+        base: "16px",
+        large: "32px",
+        xlarge: "64px",
+        xxlarge: "128px",
+        xxxlarge: "256px",
       },
     },
   },
